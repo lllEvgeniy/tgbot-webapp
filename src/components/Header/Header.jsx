336a041a1cc7255d730658d1e1a5a75b5c79e870
Evgeniy Mihievich <rashmibalajirashmi@gmail.com>
2022-10-27 18:08:15 +0300

    добавил пару тестовых кнопок

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
-import { Button } from 'bootstrap';
 import React from 'react';
 import { useTelegram } from '../hooks/useTelegram';
+import Button from '../Button/Button.jsx'
 
 const Header = () => {
-    const {user, onClose } = useTelegram()
+    const { user, onClose } = useTelegram()
     return (
         <div className={'header'}>
             <Button onClick={onClose}>Закрыть</Button>
