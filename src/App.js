d1dedb0652f6810644cd4a9e226dd11bd57123a1
Evgeniy Mihievich <rashmibalajirashmi@gmail.com>
2022-10-27 18:01:36 +0300

    добавил пару тестовых кнопок

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './components/hooks/useTelegram';
+import Header from './components/Header/Header';
 
 
 function App() {
@@ -11,7 +12,9 @@ function App() {
   }, [])
 
   return (
+
     <div className="App">
+      <Header />
       <button onClick={onToggleButton}>toggle</button>
     </div>
   );
